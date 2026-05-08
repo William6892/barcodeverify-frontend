c9dc7043979c509c0626addd8e3d646fb05d075a
William6892 <zwilliam1406@gmail.com>
2026-05-08 09:20:09 -0500

    Solucion url incorrecta backend

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 // src/services/api.ts
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5034/api';
+const API_URL = import.meta.env.VITE_API_URL || 'https://barcodeverify-backend.onrender.com/api';
 
 export const api = axios.create({
   baseURL: API_URL,
